2ea6e6220d1e1f52e2fdcc6aefc99e9326621ad4
Andrew Arrow <andrew@bird.co>
2018-10-15 00:43:45 +0000

    sincere

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import "fmt"
-
-//import "os"
-
+import "strings"
+import "os"
 import "encoding/json"
 
 func main() {
@@ -11,19 +10,24 @@ func main() {
 	//DD_API
 	//DD_APP
 	//	api_key := os.Getenv("DD_API")
-	//app_key := os.Getenv("DD_APP")
+	//application_key := os.Getenv("DD_APP")
 	//fmt.Printf("\n%s %s\n", api_key, app_key)
 
 	//https://app.datadoghq.com/api/v1/logs-queries/scopes/7156/list-csv?query=
-	fmt.Println(makeJson())
+	query := os.Args[1]
+	columns := os.Args[2]
+	fmt.Println(makeJson(query, columns))
 }
 
-func makeJson() string {
+func makeJson(query, columnString string) string {
 	time := map[string]interface{}{"offset": -7200000, "from": "now-604800s", "to": "now"}
-	search := map[string]interface{}{"query": "@thing:866 AND @otherthing:foo"}
-	f1 := map[string]interface{}{"field": map[string]interface{}{"path": "@bar"}}
-	f2 := map[string]interface{}{"field": map[string]interface{}{"path": "@msg"}}
-	columns := []interface{}{f1, f2}
+	search := map[string]interface{}{"query": query}
+	columns := []interface{}{}
+	tokens := strings.Split(columnString, " ")
+	for _, t := range tokens {
+		f := map[string]interface{}{"field": map[string]interface{}{"path": t}}
+		columns = append(columns, f)
+	}
 	t1 := map[string]interface{}{"time": map[string]interface{}{"order": "desc"}}
 	sort := map[string]interface{}{"sort": t1}
 	list := map[string]interface{}{"time": time, "search": search, "columns": columns, "limit": 5000, "sort": sort}
