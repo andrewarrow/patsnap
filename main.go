03bc1f443317b7577f6c7ea1067c0d734e3d7beb
Andrew Arrow <andrew@bird.co>
2018-10-19 20:18:05 +0000

    url

--- main.go
+++ main.go
@@ -1,43 +1,42 @@
 package main
 
 import "fmt"
+import "net/url"
+
 import "strings"
 import "os"
-import "encoding/json"
+import "time"
 
 func main() {
-	//fmt.Println("pat, snap!")
-	//DD_API
-	//DD_APP
-	//	api_key := os.Getenv("DD_API")
-	//application_key := os.Getenv("DD_APP")
-	//fmt.Printf("\n%s %s\n", api_key, app_key)
-
-	//https://app.datadoghq.com/api/v1/logs-queries/scopes/7156/list-csv?query=
-	query := os.Args[1]
-	columns := os.Args[2]
-	fmt.Println(makeJson(query, columns))
-}
-
-func makeJson(query, columnString string) string {
-	time := map[string]interface{}{"offset": -7200000, "from": "now-604800s", "to": "now"}
-	search := map[string]interface{}{"query": query}
-	columns := []interface{}{}
-	tokens := strings.Split(columnString, " ")
+	cols := os.Args[1]
+	tokens := strings.Split(cols, ",")
+	acols := []string{}
 	for _, t := range tokens {
-		f := map[string]interface{}{"field": map[string]interface{}{"path": t}}
-		columns = append(columns, f)
+		acols = append(acols, t)
 	}
-	t1 := map[string]interface{}{"time": map[string]interface{}{"order": "desc"}}
-	sort := map[string]interface{}{"sort": t1}
-	list := map[string]interface{}{"time": time, "search": search, "columns": columns, "limit": 5000, "sort": sort}
-	payload := map[string]interface{}{
-		"list": list}
+	collist := fmt.Sprintf("%v", acols)
+	fmt.Println(collist)
+	query := url.QueryEscape(os.Args[2])
+	from := time.Now().Unix() * 1000
+	from = from - 15*60*1000
+	to := time.Now().Unix() * 1000
 
-	b, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-	return (string(b))
+	u := fmt.Sprintf("https://app.datadoghq.com/logs?cols=%s&event&from_ts=%d&index=main*live=true&query=%s&stream_sort=desc&to_ts=%d", url.QueryEscape(cols), from, query, to)
+	fmt.Println(u)
+	/*
+
+
+		https://app.datadoghq.com/logs?cols=%22%5B%5C%22log_time%5C%22%2C%5C%22log_msg%5C%22%5D%22&event&from_ts=1539804802435&index=main&live=true&query=%40imei%3A866425031681804+%40dir%3Atb+%40msg%3A%2ARTO%2A&saved_view=5476&stream_sort=desc&to_ts=1539977602435
+
+			https://app.datadoghq.com/logs?cols=%22%5B%5C%22log_time%5C%22%2C%5C%22log_msg%5C%22%5D%22&event&from_ts=1539804802435&index=main&live=true&query=%40imei%3A866425031681804+%40dir%3Atb+%40msg%3A%2ARTO%2A
+
+
+			"[\"log_time\",\"log_msg\"]"&event&from_ts=1539804802435&index=main&live=true&query=@imei:866425031681804 @dir:tb @msg:*RTO*
+
+
+			&saved_view=5476
+			&stream_sort=desc
+			&to_ts=1539977602435
+
+	*/
 }
